2e93390813d2bee3d100d8cdbd2cb870eccc92da
nikaido <lr.nakamura@gmail.com>
2018-01-01 23:17:14 +0900

    Modified.

--- app.js
+++ app.js
@@ -1,17 +1,31 @@
-'use strict';
-
-const request = require('request');
-
-/**
- * Test
- */
-var main = function(){
-  let http = require('http');
-  let connect = require('connect');
-  let serveStatic = require('serve-static');
-  let app = connect().use(serveStatic('app'));
-  http.createServer(app).listen(8080);
-}
-
-
-main();
+var http = require("http");
+var socketio = require("socket.io");
+var fs = require("fs");
+ 
+var server = http.createServer(function(req, res) {
+     res.writeHead(200, {"Content-Type":"text/html"});
+     var output = fs.readFileSync("./app/index.html", "utf-8");
+     res.end(output);
+}).listen(process.env.VMC_APP_PORT || 3000);
+ 
+var io = socketio.listen(server);
+ 
+io.sockets.on("connection", function (socket) {
+ 
+  // メッセージ送信（送信者にも送られる）
+  socket.on("C_to_S_message", function (data) {
+    console.log(data);
+    io.sockets.emit("S_to_C_message", {value:data.value});
+  });
+ 
+  // ブロードキャスト（送信者以外の全員に送信）
+  socket.on("C_to_S_broadcast", function (data) {
+    console.log(data);
+    socket.broadcast.emit("S_to_C_message", {value:data.value});
+  });
+ 
+  // 切断したときに送信
+  socket.on("disconnect", function () {
+//    io.sockets.emit("S_to_C_message", {value:"user disconnected"});
+  });
+});
