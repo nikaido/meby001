48494a4dab415b7cf650b47da354f7a1da9e4fd0
nikaido <lr.nakamura@gmail.com>
2018-01-02 00:16:05 +0900

    Modified.

--- app.js
+++ app.js
@@ -1,29 +1,29 @@
 var http = require("http");
-var socketio = require("socket.io");
+var socketio = require("socket.io")(3030);
 var fs = require("fs");
- 
+
 var server = http.createServer(function(req, res) {
      res.writeHead(200, {"Content-Type":"text/html"});
      var output = fs.readFileSync("./app/index.html", "utf-8");
      res.end(output);
-}).listen(process.env.VMC_APP_PORT || 3000);
- 
+}).listen(8080);
+
 var io = socketio.listen(server);
- 
+
 io.sockets.on("connection", function (socket) {
- 
+
   // メッセージ送信（送信者にも送られる）
   socket.on("C_to_S_message", function (data) {
-    console.log(data);
+    console.log('Message:' + data);
     io.sockets.emit("S_to_C_message", {value:data.value});
   });
- 
+
   // ブロードキャスト（送信者以外の全員に送信）
   socket.on("C_to_S_broadcast", function (data) {
-    console.log(data);
+    console.log('Broadcast:' + data);
     socket.broadcast.emit("S_to_C_message", {value:data.value});
   });
- 
+
   // 切断したときに送信
   socket.on("disconnect", function () {
 //    io.sockets.emit("S_to_C_message", {value:"user disconnected"});
